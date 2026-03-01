5659e30029acd71388b160e99af31b0750dc315a
gravityrahman <105148637+gravityrahman@users.noreply.github.com>
2026-03-01 19:59:12 +0100

    Update title in server.js to 'Rahman DevOps Demo App'

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>DevOps Demo App</title>
+      <title>Rahman DevOps Demo App</title>
       <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body {
